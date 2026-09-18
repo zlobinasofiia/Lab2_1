b4f71a298dfd172f51c352f6bfb6f4227a7eda46
zlobinasofiia <zlobina.sofiia_rb61@edu.kpi.ua>
2026-09-18 16:16:13 +0300

    Зміна варіант 5

--- main.c
+++ main.c
@@ -8,17 +8,17 @@ int main()
     printf("Enter x: ");
     scanf("%lf", &x);
 
-    if( x>= 0 && x < 1)
-    y = 1.0 / (1.0 + cos(x));
+    if( x >= -20 && x < -10)
+    y = pow(2, fabs(x));
 
-    else if ( x >= 1 && x < 2 )
-    y = exp(x+2);
+    else if ( x >= -2 && x < 2 )
+    y = fabs(sin(x))/ (cos(x)+2);
 
-    else if ( x >= 4 && x < 8 )
-    y = pow(2,x)+2*x-4;
+    else if ( x >= 5 && x < 10 )
+    y = 2*x+sqrt(pow(x,2)-4);
 
     else
-    y = 0.0;
+    y = (x/5)+((x-1)/2);
 
     system("cls");
 
